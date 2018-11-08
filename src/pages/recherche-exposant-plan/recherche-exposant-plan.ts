2f7fb045421da7e25614df3a011d61bfe1ab6078
SERT Mikayil <mikayil.sert@see.local>
2018-11-08 14:50:30 +0100

    Modifications des requette sql.

--- src/pages/recherche-exposant-plan/recherche-exposant-plan.ts
+++ src/pages/recherche-exposant-plan/recherche-exposant-plan.ts
@@ -39,8 +39,8 @@ export class RechercheExposantPlanPage implements OnInit
   ngOnInit()
   {
     // Charge les thèmes
-    let sql = "select id, libelle from THEME order by libelle" ;
-    this.sqlPrd.select( sql, null, this.themes ) ;
+    let sql = "select id, libelle from theme_18 order by libelle";
+    this.sqlPrd.select( sql, null, this.themes );
 
   }
 
@@ -51,41 +51,71 @@ export class RechercheExposantPlanPage implements OnInit
 
     if (this.recherche.libelle && this.recherche.themeId)
     {
-      console.log("ok");
+      let libelle = this.recherche.libelle.toLocaleUpperCase();
+
+      let sql = "SELECT stand_18.id as idStand, exposant_18.id, exposant_18.nom "
+      sql += "FROM exposant_18 "
+      sql += "JOIN presenter_18 ON exposant_18.id = presenter_18.idExposant "
+      sql += "JOIN theme_18 ON presenter_18.idTheme = theme_18.id "
+      sql += "JOIN etresur_18 ON exposant_18.id = etresur_18.idExposant "
+      sql += "JOIN stand_18 ON etresur_18.idStand = stand_18.id "
+      sql += "WHERE theme_18.id = " + this.recherche.themeId + " AND exposant_18.nom LIKE '%" + libelle + "%' "
+      sql += " ORDER BY exposant_18.nom"
+
+      this.sqlPrd.select(sql, null, this.exposants).then((data) => {
+        this.exposants.forEach((e) => {
+          this.marqueurs.push(new PlanMarqueur(e.idStand, e.nom))
+        })
+      })
+
     }
     else if( this.recherche.libelle )
     {
-      let libelle = '%' + this.recherche.libelle.toLocaleUpperCase() + '%'
-      let sql;
-      sql += "select numstand, id, libelle"
-      sql += "from EXPOSANT, EXPOSER"
-      sql += "where id = idExposant and lower(libelle)"
-      sql += "like ? order by libelle"
-      this.sqlPrd.select( sql, [libelle], this.exposants ).then((data)=>
+      let libelle = this.recherche.libelle.toLocaleUpperCase();
+
+      let sql = "SELECT stand_18.id, exposant_18.id, exposant_18.nom "
+      sql += "FROM exposant_18 "
+      sql += "JOIN etresur_18 ON exposant_18.id = etresur_18.idExposant "
+      sql += "JOIN stand_18 ON etresur_18.idStand = stand_18.id "
+      sql += "WHERE exposant_18.nom LIKE '%" + libelle + "%' "
+      sql += "ORDER BY exposant_18.nom"
+      
+      this.sqlPrd.select( sql, null, this.exposants ).then((data)=>
       {
+        console.log(sql);
         this.exposants.forEach( (e)=>
         {
-          this.marqueurs.push( new PlanMarqueur( e.numstand, e.libelle) ) ;
+          this.marqueurs.push( new PlanMarqueur( e.idStand, e.nom) ) ;
         }) ;
       }) ;
     }
     else if( this.recherche.themeId )
     {
-      let sql = "select numstand, id, libelle" ;
-      sql += " from EXPOSANT as et, EXPOSER as er, CONCERNE as c" ;
-      sql += " where et.id=er.idExposant and et.id=c.idExposant and c.idTheme=?" ;
-      sql += " order by libelle" ;
-      this.sqlPrd.select( sql , [this.recherche.themeId], this.exposants ).then((data)=>
+      let sql = "SELECT stand_18.id as idStand, exposant_18.id, exposant_18.nom "
+      sql += "FROM exposant_18 "
+      sql += "JOIN presenter_18 ON exposant_18.id = presenter_18.idExposant "
+      sql += "JOIN theme_18 ON presenter_18.idTheme = theme_18.id "
+      sql += "JOIN etresur_18 ON exposant_18.id = etresur_18.idExposant "
+      sql += "JOIN stand_18 ON etresur_18.idStand = stand_18.id "
+      sql += "WHERE theme_18.id = " + this.recherche.themeId
+      sql += " ORDER BY exposant_18.nom"
+
+      console.log(this.recherche.themeId)
+
+      this.sqlPrd.select( sql , null, this.exposants ).then((data)=>
       {
         this.exposants.forEach( (e)=>
         {
-          this.marqueurs.push( new PlanMarqueur( e.numstand, e.libelle) ) ;
+          this.marqueurs.push( new PlanMarqueur( e.idStand, e.nom) ) ;
         }) ;
       }) ;      
     }
     else
     {
-      let sql = "select idStand, id, nom from EXPOSANTS_18 JOIN etresur_18 ON id=idExposant order by nom"
+      let sql = "select idStand, id, nom "
+      sql += "from EXPOSANTS_18 "
+      sql += "JOIN etresur_18 ON id = idExposant " 
+      sql += "order by nom"
       this.sqlPrd.select( sql , null, this.exposants ) ;
     }
   }
