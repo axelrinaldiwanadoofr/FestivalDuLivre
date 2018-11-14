f82b9617983c4eb7def01acccb932eaeaf3c71bd
LITZLER David Joseph <david.litzler@see.local>
2018-11-14 15:12:05 +0100

    finalisation de l'affichage des énigmes

--- src/pages/jeuPiste/enigmes/enigme1/enigme1.ts
+++ src/pages/jeuPiste/enigmes/enigme1/enigme1.ts
@@ -18,6 +18,7 @@ export class Enigme1 {
   public erreurRep: boolean;
   public reponseValidee: boolean;
   public repAValider: boolean;
+  public afficherIntitulé: boolean;
 
   public codeEnigme: string;
   public reponseDonnee: string;
@@ -28,26 +29,46 @@ export class Enigme1 {
       //Jour 1 
       //Enigme 1
       {
-        title: "Dans la vigie...", description:
+        title: "Il était une fois...", description:
           "Toujours le premier, <br/> Jamais le dernier.<br/> Rien sans lui n'est jamais fini, <br/> Car rien ne peut commencer sans lui.<br/><br/>  Qui est-il ?",
-        day: 1, numStand: 100, reponse: ["début", "debut", "le début", "le debut", "DEBUT", "LE DEBUT", "Le Debut", "Le Début", "le Début", "le Debut"], code: "md7at1", commentaireFin: "Le début amorce l'histoire et ne permet pas de finir quoi que ce soit s'il n'est pas présent. "
+        day: 1, numStand: 100, reponse: ["début", "debut", "le début", "le debut", "DEBUT", "LE DEBUT", "Le Debut", "Le Début", "le Début", "le Debut, Le début"], 
+        code: "md7at1", commentaireFin: "Le début amorce l'histoire et ne permet pas de finir quoi que ce soit s'il n'est pas présent. "
       },
       //Enigme 2
-      { title: "coucou", description: "ldjfldj", day: 1, numStand: 201, reponse: [" "], code: "h5cr2h", commentaireFin: " " },
+      { title: "AVOCAT", description: 
+          "Déchiffrez le code : <br/>OHYBNO",
+           day: 1, numStand: 201, reponse: ["EXORDE", "exorde", "éxorde", "l'exorde", "L'exorde", "l'éxorde", "L'éxorde"], code: "h5cr2h",
+            commentaireFin: " Definition : <br/> Première partie d'un discours.<br/> Exorde d'une harangue, d'un plaidoyer. " },
+      //Enigme 3
       { title: "coucou", description: "ldjfldj", day: 1, numStand: 340, reponse: [" "], code: "4gla2x", commentaireFin: " " },
+      //Enigme 4
       { title: "coucou", description: "ldjfldj", day: 1, numStand: 426, reponse: [" "], code: "yf8t2d", commentaireFin: " " },
+      //Enigme 5
       { title: "coucou", description: "ldjfldj", day: 1, numStand: 500, reponse: [" "], code: "nhe8wz", commentaireFin: " " },
+      //Enigme 6
       { title: "coucou", description: "ldjfldj", day: 1, numStand: 416, reponse: [" "], code: "f7ezf5", commentaireFin: " " },
+      //Enigme 7
       { title: "coucou", description: "ldjfldj", day: 1, numStand: 320, reponse: [" "], code: "kf8s3y", commentaireFin: " " },
+      //Enigme 8
       { title: "coucou", description: "ldjfldj", day: 1, numStand: 235, reponse: [" "], code: "4frt8d", commentaireFin: " " },
+
+
       //Jour 2 
+      //Enigme 1
       { title: "Dans la vigie...", description: "ldjfldj", day: 2, numStand: 100, reponse: [" "], code: "", commentaireFin: " " },
+      //Enigme 2
       { title: "coucou", description: "ldjfldj", day: 2, numStand: 201, reponse: [" "], code: "", commentaireFin: " " },
+      //Enigme 3
       { title: "coucou", description: "ldjfldj", day: 2, numStand: 340, reponse: [" "], code: "", commentaireFin: " " },
+      //Enigme 4
       { title: "coucou", description: "ldjfldj", day: 2, numStand: 426, reponse: [" "], code: "", commentaireFin: " " },
+      //Enigme 5
       { title: "coucou", description: "ldjfldj", day: 2, numStand: 500, reponse: [" "], code: "", commentaireFin: " " },
+      //Enigme 6
       { title: "coucou", description: "ldjfldj", day: 2, numStand: 416, reponse: [" "], code: "", commentaireFin: " " },
+      //Enigme 7
       { title: "coucou", description: "ldjfldj", day: 2, numStand: 320, reponse: [" "], code: "", commentaireFin: " " },
+      //Enigme 8
       { title: "coucou", description: "ldjfldj", day: 2, numStand: 235, reponse: [" "], code: "", commentaireFin: " " }
     ];
 
@@ -59,6 +80,7 @@ export class Enigme1 {
     this.reponseBonne = false;
     this.reponseValidee = false;
     this.repAValider = false;
+    this.afficherIntitulé = false;
 
 
     this.todo = this.lesEnigmes[this.index];
@@ -81,9 +103,10 @@ export class Enigme1 {
     this.todo.reponse.forEach(rep => {
       if ((rep == this.reponseDonnee || this.reponseBonne)) {
         this.erreurRep = false;
+        this.codeBon = true;
         this.reponseBonne = true;
         this.codeEnigme = "";
-        this.codeBon = false;
+        this.repAValider = false;
       }
       else {
         this.erreurRep = true;
@@ -97,5 +120,6 @@ export class Enigme1 {
     this.compteurReponse ++;
     this.index ++;
     this.todo = this.lesEnigmes[this.index];
+    this.codeBon = false;
   }
 }
