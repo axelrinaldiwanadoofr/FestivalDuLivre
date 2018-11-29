7f9b3cb07b90c2e4b9155f6375f2e5eb2aec3b67
Koroppaisensei <ywalch@gmail.com>
2018-11-29 13:52:52 +0100

    code de "livrePage" et "recherche-livre" commenté

--- src/pages/recherche-livre/recherche-livre.ts
+++ src/pages/recherche-livre/recherche-livre.ts
@@ -46,7 +46,7 @@ export class RechercheLivrePage implements OnInit {
     let sql = "SELECT * FROM theme_18 ORDER BY libelle";
     this.sqlPrd.select(sql, null, this.themes);
 
-    // Charge les tranches d'ages
+    // Charge les tranches d'âges
     sql = "SELECT * FROM trancheage_18 ORDER BY id";
     this.sqlPrd.select(sql, null, this.trancheAges);
 
@@ -63,56 +63,67 @@ export class RechercheLivrePage implements OnInit {
     // this.sqlPrd.select(sql, this.stands);
 
     // Afficher la liste dès l'arrivée sur la page
-    sql = "SELECT l.id as id, titre, editeur, auteur, libelle, l.image as image, nom, idStand AS numStand ";
-    sql += "FROM livre_18 as l, theme_18 as t, exposant_18 as e, etresur_18 AS et ";
+    sql = "SELECT l.id AS id, titre, editeur, auteur, libelle, l.image AS image, nom, idStand AS numStand ";
+    sql += "FROM livre_18 AS l, theme_18 AS t, exposant_18 AS e, etresur_18 AS et ";
     sql += "WHERE l.idTheme = t.id and l.idExposant = e.id "
     sql += "AND et.idExposant = l.idExposant " ;
     sql += "ORDER BY l.titre" ;
     this.sqlPrd.select( sql, [], this.livres) ;
   }
 
+
+  // Au clic, affiche la liste en fonction des informations renseignées par l'utilisateur en
   onRechercheClick() {
     this.livres = [];
-
+    
     let sql = "SELECT l.id AS id, titre, editeur, auteur, libelle, l.image AS image, nom, idTrancheAge, idStand AS numStand ";
-    sql += "FROM livre_18 aS l, theme_18 AS t, exposant_18 AS e, etresur_18 AS et ";
+    sql += "FROM livre_18 AS l, theme_18 AS t, exposant_18 AS e, etresur_18 AS et ";
     sql += "WHERE l.idTheme = t.id and l.idExposant = e.id ";
     sql += "AND et.idExposant = l.idExposant ";
-    // requête si titre rempli
+
+    // Si titre rempli, prendre en compte ce qui est entré
     if (this.recherche.titre ) 
     {
       sql += " AND titre LIKE '" + '%' + this.recherche.titre + '%' + "' " ;
     }
-  // requête si auteur rempli
+
+  // Si auteur rempli et que "tous les auteurs" n'est pas sélectionné, prendre en compte l'auteur
     if (this.recherche.auteur && this.recherche.auteur != "0" ) 
     {
       sql += " AND auteur LIKE '" + '%' + this.recherche.auteur + '%' + "' " ;
     }
-    // requête si editeur rempli
+
+    // Si editeur rempli et que "tous les editeurs" n'est pas sélectionné, prendre en compte l'editeur
     if (this.recherche.editeur && this.recherche.editeur != "0" ) 
     {
       sql += " AND editeur LIKE '" + '%' + this.recherche.editeur + '%' + "' " ;
     }
-    // requête si theme rempli
+
+    // Si thème rempli et que "aucun" n'est pas sélectionné, prendre en compte le thème
     if( this.recherche.themeId && this.recherche.themeId != "0" ) 
     {
       sql += " AND idTheme=" + this.recherche.themeId ;
     }
-    // requête si tranche d'âge rempli
+
+    // Si tranche d'âge rempli et que "toutes les tranches d'âge" n'est pas sélectionné, prendre en compte la tranche d'âge
     if( this.recherche.trancheAgeId && this.recherche.trancheAgeId != "0" ) 
     {
       sql += " AND idTrancheAge=" + this.recherche.trancheAgeId ;
     }
-    // classé par titre
+
+    // classer par titre
     sql += " ORDER BY titre" ;
     this.sqlPrd.select(sql, null, this.livres);
   }
 
-  // si clic sur un livre de la liste affichée par la requête
+
+  // si clic sur un livre de la liste affichée par la requête, dirige vers LivrePage du livre cliqué
   onLivreClick(livre) {
     this.navCtrl.push(LivrePage, { idLivre: livre.id });
   }
 
+
+  // bouton accueil
   Accueil() {
     this.navCtrl.setRoot(HelloIonicPage);
   }
